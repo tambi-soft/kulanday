fc723632af5cb3c319ea778590f1845c9c60c646
nano13 <nano13@gmx.net>
2018-04-10 18:56:05 +0200

    init deck_item_widget

---
+++ src/q_deck_item_widget.cpp
@@ -0,0 +1,6 @@
+#include "q_deck_item_widget.h"
+
+QDeckItemWidget::QDeckItemWidget(QWidget *parent) : QWidget(parent)
+{
+    
+}
---
+++ src/q_deck_item_widget.h
@@ -0,0 +1,17 @@
+#ifndef Q_DECK_ITEM_WIDGET_H
+#define Q_DECK_ITEM_WIDGET_H
+
+#include <QWidget>
+
+class QDeckItemWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit QDeckItemWidget(QWidget *parent = nullptr);
+    
+signals:
+    
+public slots:
+};
+
+#endif // Q_DECK_ITEM_WIDGET_H
