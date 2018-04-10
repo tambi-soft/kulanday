2e0e6f0aa5cefe2b174f27434e17ba27e609c2d8
nano13 <nano13@gmx.net>
2018-04-11 00:05:39 +0200

    progress on audio list

--- src/q_audio_list_widget.h
+++ src/q_audio_list_widget.h
@@ -2,12 +2,23 @@
 #define Q_AUDIO_LIST_WIDGET_H
 
 #include <QWidget>
+#include <QMediaPlayer>
 
 class QAudioListWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit QAudioListWidget(QWidget *parent = nullptr);
+    
+private:
+    int PLAY_BUTTON_COLUMN = 1;
+    int RECORD_BUTTON_COLUMN = 1;
+    int DELETE_BUTTON_COLUMN = 2;
+    int OPEN_FILE_BUTTON_COLUMN = 3;
+    int EDIT_FILE_BUTTON = 4;
+    int FILE_NAME_COLUMN = 5;
+    
+    
     
 signals:
     
