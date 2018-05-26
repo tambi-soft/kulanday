0953b41b2f51c14f549900a42d5a8e7fba74ad79
nano13 <nano13@gmx.net>
2018-05-27 00:34:47 +0200

    introducing new class search

---
+++ src/q_search_widget.cpp
@@ -0,0 +1,6 @@
+#include "q_search_widget.h"
+
+QSearchWidget::QSearchWidget(QWidget *parent) : QWidget(parent)
+{
+    
+}
---
+++ src/q_search_widget.h
@@ -0,0 +1,17 @@
+#ifndef Q_SEARCH_WIDGET_H
+#define Q_SEARCH_WIDGET_H
+
+#include <QWidget>
+
+class QSearchWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit QSearchWidget(QWidget *parent = nullptr);
+    
+signals:
+    
+public slots:
+};
+
+#endif // Q_SEARCH_WIDGET_H
